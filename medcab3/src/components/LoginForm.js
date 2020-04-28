cacf969214486e952046aad24cc2e197cc650f26
evan <aspaasevan@gmail.com>
2020-04-28 14:56:12 -0700

    refactor of login form in place

--- medcab3/src/components/LoginForm.js
+++ medcab3/src/components/LoginForm.js
@@ -1,43 +1,57 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { axiosWithAuth } from "../utils/axiosAuth";
+import styled from "styled-components";
 
 export const LoginFormDiv = styled.div`
-    background-color: #EDFFEA
-    
-`
+  background-color: #edffea;
+`;
 
-export default function LoginForm(props) {
-    // leaving handlers for state change here
-    const {
-        values,
-        onInputChange,
-        onSubmit
-    } = props
+export default function LoginForm() {
+	const [credentials, setCredentials] = useState({
+		username: "",
+		password: ""
+	});
 
-    return (
-        <form className='form' id='login-form'>
-            <p>Please sign in to coninue.</p>
-            <h6>Email:</h6>
-            <label>
-                <input
-                    value={values.email}
-                    onChange={onInputChange}
-                    name='email'
-                    type='text'
-                />
-            </label>
-            <h6>Password:</h6>
-            <label>
-                <input
-                    value={values.password}
-                    onChange={onInputChange}
-                    name='password'
-                    type='text'
-                />
-            </label>
-            <button onClick={onSubmit}>Login</button>
+  handleChange = e => {
+		setCredentials({
+			...credentials, 
+			[e.target.name]: e.target.value
+		})
+    // this.setState({
+    //   credentials: {
+    //     ...this.state.credentials,
+    //     [e.target.name]: e.target.value
+    //   }
+    // });
+  };
 
+  login = e => {
+		e.preventDefault();
+		console.log('inside submit for login')
+  };
 
-        </form>
-    )
+  return (
+    <form onSubmit={login} className="form" id="login-form">
+			<p>Please sign in to coninue.</p>
+      <h6>Email:</h6>
+      <label htmlFor="email">
+				<input
+						value={credentials.email}
+						onChange={handleChange}
+						name="email"
+						type="text"
+					/>
+			</label>
+      <h6>Password:</h6>
+      <label>
+        <input
+          value={values.password}
+          onChange={onInputChange}
+          name="password"
+          type="text"
+        />
+      </label>
+			<button type="submit">Log in</button>
+		</form>
+  );
 }
